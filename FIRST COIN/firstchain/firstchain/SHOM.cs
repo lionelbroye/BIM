5f487c4c8de7ab2b5f87ce18a8b0be9dc3634845
gggraph <62741099+gggraph@users.noreply.github.com>
2020-09-23 13:49:42 +0200

    Update SHOM.cs

--- FIRST COIN/firstchain/firstchain/SHOM.cs
+++ FIRST COIN/firstchain/firstchain/SHOM.cs
@@ -28,6 +28,14 @@ namespace firstchain
                 this.timestamp = ts;
             
             }
+
+            public void print()
+            {
+                Console.WriteLine("id de la station = " + this.idstation);
+                Console.WriteLine("hauteur de l'eau = " + this.value);
+                Console.WriteLine("Timestamp        = " + this.timestamp.ToString());
+
+            }
         }
 
         public static void GetSHOMData(int station, DateTime dateStart, DateTime dateEnd)
@@ -71,7 +79,6 @@ namespace firstchain
                 if (sLine != null)
                     dtreceived = sLine;
             }
-           // Console.WriteLine(dtreceived);
 
             // ----------------------------------------> GET LAST DATA
 
@@ -80,14 +87,112 @@ namespace firstchain
             Console.WriteLine("     Latest data received      ");
             Console.WriteLine("_______________________________");
             SHOMData lastresult = GetLastData(dtreceived.ToCharArray());
-            Console.WriteLine("id de la station = " + lastresult.idstation);
-            Console.WriteLine("hauteur de l'eau = " + lastresult.value);
-            Console.WriteLine("Timestamp        = " + lastresult.timestamp.ToString());
-
-
+            lastresult.print();
+            // 11/15/20 ... lol ... 
+            Console.WriteLine("_______________________________");
+            Console.WriteLine("   Specific data at 11:15:00   ");
+            Console.WriteLine("_______________________________");
+            DateTime datedesired = new DateTime(dateStart.Year, dateStart.Month, dateStart.Day, 11, 15, 0);
+            SHOMData searchresult = GetDataAtSpecificTime(datedesired, dtreceived.ToCharArray());
+            if ( searchresult != null)
+            {
+                searchresult.print();
+            }
+            else
+            {
+                Console.WriteLine("not found...");
+            }
         }
 
-        
+
+        public static SHOMData StringToSHOMData(string result)
+        {
+            //{"idstation":22,"idsource":1,"value":0.6337,"timestamp":"2020/09/23 10:37:20"}]}
+
+
+            int idstation = 0;
+            int idsource = 0;
+            float value = 0;
+            string[] parser = result.Split(',');
+            int.TryParse(parser[0].Replace("{\"idstation\":", ""), out idstation);
+            int.TryParse(parser[1].Replace("\"idsource\":", ""), out idsource);
+            string fparsing = parser[2].Replace('.', ',');
+            float.TryParse(fparsing.Replace("\"value\":", ""), out value);
+            // parse the space --- >  
+            string tsparsing = parser[3].Replace("\"timestamp\":\"", "");
+            tsparsing = tsparsing.Replace("\"}]}", "");
+            // now i have 2020/09/23 10:37:20
+            string[] YMD = tsparsing.Split(' ')[0].Split('/');
+            int year = 0;
+            int month = 0;
+            int day = 0;
+            int.TryParse(YMD[0], out year);
+            int.TryParse(YMD[1], out month);
+            int.TryParse(YMD[2], out day);
+            DateTime timestamp = new DateTime(year, month, day);
+            string[] HMS = tsparsing.Split(' ')[1].Split(':');
+            int hour = 0;
+            int minute = 0;
+            int second = 0;
+            int.TryParse(HMS[0], out hour);
+            int.TryParse(HMS[1], out minute);
+            int.TryParse(HMS[2], out second);
+            timestamp = timestamp.AddHours(hour);
+            timestamp = timestamp.AddMinutes(minute);
+            timestamp = timestamp.AddSeconds(second);
+            return new SHOMData(idstation, idsource, value, timestamp);
+        }
+
+        public static List<SHOMData> GetAllSHOMData(char[] data)
+        {
+            List<SHOMData> resultdata = new List<SHOMData>();
+            int latestOpenedBrackets = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == '{')
+                {
+                    latestOpenedBrackets = i;
+                    List<char> stringBuild = new List<char>();
+                    int bracketcount = 0;
+                    for (int n = latestOpenedBrackets; n < data.Length; n++)
+                    {
+                        if (data[n] == '}')
+                        {
+                            bracketcount++;
+                        }
+                        if (bracketcount == 2)
+                        {
+                            break;
+                        }
+                        stringBuild.Add(data[n]);
+                    }
+                    string result = "";
+                    foreach (char c in stringBuild)
+                    {
+
+                        result += c;
+                    }
+                    SHOMData sd = StringToSHOMData(result);
+                    resultdata.Add(sd);
+
+                }
+            }
+            return resultdata;
+        }
+        public static SHOMData GetDataAtSpecificTime(DateTime dt, char[] data)
+        {
+            List<SHOMData> resultdata = GetAllSHOMData(data);
+            foreach( SHOMData shom in resultdata)
+            {
+                int cp = DateTime.Compare(dt, shom.timestamp);
+                if ( cp == 0)
+                {
+                    return shom;
+                }
+            }
+
+            return null;
+        }
 
         public static SHOMData GetLastData(char[] data)
         {
@@ -107,43 +212,12 @@ namespace firstchain
             string result = "";
             foreach ( char c in stringBuild)
             {
+                
                 result += c; 
             }
-
-            //{"idstation":22,"idsource":1,"value":0.6337,"timestamp":"2020/09/23 10:37:20"}]}
-
-            
-            int idstation = 0;
-            int idsource = 0;
-            float value = 0;
-            string[] parser = result.Split(',');
-            int.TryParse(parser[0].Replace("{\"idstation\":", ""), out idstation);
-            int.TryParse(parser[1].Replace("\"idsource\":", ""), out idsource);
-            string fparsing = parser[2].Replace('.', ',');
-            float.TryParse(fparsing.Replace("\"value\":", ""), out value);
-            // parse the space --- >  
-            string tsparsing = parser[3].Replace("\"timestamp\":\"", "");
-            tsparsing = tsparsing.Replace("\"}]}", "");
-            // now i have 2020/09/23 10:37:20
-            string[] YMD = tsparsing.Split(' ')[0].Split('/') ;
-            int year = 0;
-            int month = 0;
-            int day = 0;
-            int.TryParse(YMD[0], out year);
-            int.TryParse(YMD[1], out month);
-            int.TryParse(YMD[2], out day);
-            DateTime timestamp = new DateTime(year, month, day);
-            string[] HMS = tsparsing.Split(' ')[1].Split(':');
-            int hour = 0;
-            int minute = 0;
-            int second = 0;
-            int.TryParse(HMS[0], out hour);
-            int.TryParse(HMS[1], out minute);
-            int.TryParse(HMS[2], out second);
-            timestamp = timestamp.AddHours(hour);
-            timestamp = timestamp.AddMinutes(minute);
-            timestamp = timestamp.AddSeconds(second);
-            return new SHOMData(idstation, idsource, value, timestamp);
+            SHOMData sd = StringToSHOMData(result);
+            return sd;
+           
         }
 
     }
