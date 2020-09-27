63277453c7be2bf721af5e4124bd59ea23f42607
gggraph <62741099+gggraph@users.noreply.github.com>
2020-09-28 00:45:58 +0200

    Add files via upload

---
+++ Pixel Extraction Chain ( fork )/drawprogram/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace drawprogram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mouseXYinfo = new System.Windows.Forms.Label();
+            this.pixelCountInfo = new System.Windows.Forms.Label();
+            this.checkpixelstateBTN = new System.Windows.Forms.Button();
+            this.proccessingpixelInfo = new System.Windows.Forms.Label();
+            this.validationInfo = new System.Windows.Forms.TextBox();
+            this.myUPOInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mouseXYinfo
+            // 
+            this.mouseXYinfo.AutoSize = true;
+            this.mouseXYinfo.Location = new System.Drawing.Point(60, 428);
+            this.mouseXYinfo.Name = "mouseXYinfo";
+            this.mouseXYinfo.Size = new System.Drawing.Size(22, 13);
+            this.mouseXYinfo.TabIndex = 0;
+            this.mouseXYinfo.Text = "0:0";
+            // 
+            // pixelCountInfo
+            // 
+            this.pixelCountInfo.AutoSize = true;
+            this.pixelCountInfo.Location = new System.Drawing.Point(697, 389);
+            this.pixelCountInfo.Name = "pixelCountInfo";
+            this.pixelCountInfo.Size = new System.Drawing.Size(13, 13);
+            this.pixelCountInfo.TabIndex = 1;
+            this.pixelCountInfo.Text = "0";
+            // 
+            // checkpixelstateBTN
+            // 
+            this.checkpixelstateBTN.Location = new System.Drawing.Point(700, 13);
+            this.checkpixelstateBTN.Name = "checkpixelstateBTN";
+            this.checkpixelstateBTN.Size = new System.Drawing.Size(75, 23);
+            this.checkpixelstateBTN.TabIndex = 2;
+            this.checkpixelstateBTN.Text = "pixel state";
+            this.checkpixelstateBTN.UseVisualStyleBackColor = true;
+            this.checkpixelstateBTN.Click += new System.EventHandler(this.checkpixelstateBTN_Click);
+            // 
+            // proccessingpixelInfo
+            // 
+            this.proccessingpixelInfo.AutoSize = true;
+            this.proccessingpixelInfo.Location = new System.Drawing.Point(697, 415);
+            this.proccessingpixelInfo.Name = "proccessingpixelInfo";
+            this.proccessingpixelInfo.Size = new System.Drawing.Size(13, 13);
+            this.proccessingpixelInfo.TabIndex = 3;
+            this.proccessingpixelInfo.Text = "0";
+            // 
+            // validationInfo
+            // 
+            this.validationInfo.Location = new System.Drawing.Point(660, 56);
+            this.validationInfo.Multiline = true;
+            this.validationInfo.Name = "validationInfo";
+            this.validationInfo.Size = new System.Drawing.Size(128, 294);
+            this.validationInfo.TabIndex = 4;
+            // 
+            // myUPOInfo
+            // 
+            this.myUPOInfo.AutoSize = true;
+            this.myUPOInfo.Location = new System.Drawing.Point(300, 428);
+            this.myUPOInfo.Name = "myUPOInfo";
+            this.myUPOInfo.Size = new System.Drawing.Size(35, 13);
+            this.myUPOInfo.TabIndex = 5;
+            this.myUPOInfo.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.myUPOInfo);
+            this.Controls.Add(this.validationInfo);
+            this.Controls.Add(this.proccessingpixelInfo);
+            this.Controls.Add(this.checkpixelstateBTN);
+            this.Controls.Add(this.pixelCountInfo);
+            this.Controls.Add(this.mouseXYinfo);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "PAINTMYBLOCKCHAIN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mouseXYinfo;
+        private System.Windows.Forms.Label pixelCountInfo;
+        private System.Windows.Forms.Button checkpixelstateBTN;
+        private System.Windows.Forms.Label proccessingpixelInfo;
+        public System.Windows.Forms.TextBox validationInfo;
+        private System.Windows.Forms.Label myUPOInfo;
+    }
+}
+
---
+++ Pixel Extraction Chain ( fork )/drawprogram/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace drawprogram
+{
+    public partial class Form1 : Form
+    {  
+        Bitmap myDraw = new Bitmap(1000, 1000); // only allow a 1000 on 1000 bitmap ( but it doesnt matter ) .... 
+        Graphics g;
+        PictureBox drawBox = new PictureBox();
+        public static string _folderPath = "";
+        public List<PixelWrapper.Vector> UnvalidatedPixel = new List<PixelWrapper.Vector>();
+        public static List<List<PixelWrapper.Vector>> ProccessedPixel = new List<List<PixelWrapper.Vector>>();
+        public List<PixelWrapper.Pixel> ValidatedPixel = new List<PixelWrapper.Pixel>();
+        public uint pixelCount = 0;
+        public uint pixelProcessedCount = 0;
+        public bool _checkvalidity = true;
+
+        public bool ctrl_pressed = false;
+        public bool _MouseDown = false;
+        public List<PixelWrapper.Vector> MousePos = new List<PixelWrapper.Vector>();
+        public PixelWrapper.Vector SelectPOINTA = null;
+        public PixelWrapper.Vector SelectPOINTB = null;
+
+        public string KeyPath = "";
+        public static PixelWrapper.UPO MYUPO = null;
+        // get private and public key from path ... 
+        public Form1()
+        {
+            _folderPath = "C:\\Users\\Gaël\\source\\repos\\PIXCHAIN\\firstchain\\bin\\Debug\\";
+            KeyPath = "C:\\Users\\Gaël\\source\\repos\\PIXCHAIN\\firstchain\\bin\\Debug\\Compte A\\";
+            if ( !File.Exists(KeyPath + "publicKey")) { MessageBox.Show("keys are not in folder! Please Relaunch!");  return;  }
+            ulong mupoindex = PixelWrapper.GetUPOIndex(PixelWrapper.ComputeSHA256(File.ReadAllBytes(KeyPath + "publicKey")));
+            if ( mupoindex == 0) { MessageBox.Show("Upo not found! Please Mine something and relaunch!"); return; }
+            MYUPO = PixelWrapper.GetOfficialUPO_B(mupoindex);
+            if ( MYUPO == null)
+            {
+                MessageBox.Show("Upo not found! Please Mine something and relaunch!"); return;
+
+            }
+            
+            InitializeComponent();
+            myUPOInfo.Text = PixelWrapper.SHAToHex(MYUPO.HashKey, false) + " | sold : " + MYUPO.Sold;
+            g = Graphics.FromImage(myDraw);
+            this.Controls.Add(drawBox);
+            drawBox.Size = new Size(1000, 1000);
+            
+            drawBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDown);
+            drawBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MouseUp);
+            drawBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Draw);
+            this.KeyDown += KeyDownHandler;
+            this.KeyUp += KeyUpHandler;
+
+            Thread validationThread = new Thread(new ThreadStart(PixelWrapper.ValidatingThread));
+            validationThread.IsBackground = true;
+            validationThread.Start();
+        }
+
+        public void KeyDownHandler(object sender, KeyEventArgs e)
+        {
+
+            if ( e.KeyCode == Keys.D && !ctrl_pressed)
+            {
+                ctrl_pressed = true;
+                SelectPOINTA = null;
+                SelectPOINTB = null;
+            }
+        }
+        public void KeyUpHandler(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.D)
+            {
+                ctrl_pressed = false;
+                ClearDraw();
+                PrintAllPixel();
+                if (SelectPOINTA != null)
+                {
+                    if (SelectPOINTB.X < SelectPOINTA .X || SelectPOINTB.Y < SelectPOINTA.Y)
+                    {
+                       
+                        return;
+                    }
+                    else
+                    {
+                        List<PixelWrapper.Vector> selpix = new List<PixelWrapper.Vector>();
+                        for (int y = SelectPOINTA.Y ; y <= SelectPOINTB.Y; y++)
+                        {
+                            for (int x = SelectPOINTA.X; x <= SelectPOINTB.X; x++)
+                            {
+                                for ( int i = 0; i < UnvalidatedPixel.Count; i++ )
+                                {
+                                    if (UnvalidatedPixel[i].X == x && UnvalidatedPixel[i].Y == y)
+                                    {
+                                        selpix.Add(UnvalidatedPixel[i]);
+                                        UnvalidatedPixel.RemoveAt(i) ;
+                                        break;
+                                        // remove pix then ... 
+                                    }
+                                }
+                            }
+
+                        }
+                        
+                        ProccessedPixel.Add(selpix);
+                        pixelProcessedCount += (uint)selpix.Count;
+                        proccessingpixelInfo.Text = pixelProcessedCount.ToString();
+                        PrintAllPixel();
+                    }
+                       
+                }
+               
+            }
+        }
+        public void ClearDraw()
+        {
+            myDraw = new Bitmap(1000, 1000);
+            g = Graphics.FromImage(myDraw);
+        }
+        public void PrintAllPixel()
+        {
+            foreach (PixelWrapper.Vector pix in UnvalidatedPixel)
+            {
+                g.FillRectangle(Brushes.Red, pix.X, pix.Y, 1, 1);
+                
+            }
+            foreach ( List<PixelWrapper.Vector> proclist in ProccessedPixel)
+            {
+                foreach (PixelWrapper.Vector pix in proclist)
+                {
+                    g.FillRectangle(Brushes.Blue, pix.X, pix.Y, 1, 1);
+                }
+            }
+            foreach (PixelWrapper.Pixel vpix in ValidatedPixel)
+            {
+                g.FillRectangle(Brushes.Black, vpix.position.X, vpix.position.Y, 1, 1);
+                drawBox.Image = myDraw;
+            }
+            drawBox.Image = myDraw;
+        }
+        public void PrintValidatedPixel()
+        {
+            ClearDraw();
+            _checkvalidity = true;
+            foreach ( PixelWrapper.Pixel vpix in ValidatedPixel)
+            {
+                g.FillRectangle(Brushes.Black, vpix.position.X, vpix.position.Y, 1, 1);
+                drawBox.Image = myDraw;
+            }
+        }
+
+        private void MouseDown(object sender, MouseEventArgs e)
+        {
+            _MouseDown = true ;
+        }
+        private void MouseUp(object sender, MouseEventArgs e)
+        {
+            _MouseDown = false;
+            if ( _checkvalidity )
+            {
+                PrintAllPixel();
+                _checkvalidity = false;
+            }
+        }
+
+        private void Draw(object sender, MouseEventArgs e)
+        {
+            if ( _MouseDown && !ctrl_pressed)
+            {
+                g.FillRectangle(Brushes.Red, e.X, e.Y, 1, 1);
+                drawBox.Image = myDraw;
+                mouseXYinfo.Text = e.X.ToString() + ":" + e.Y.ToString();
+                PixelWrapper.Vector upix = new PixelWrapper.Vector(e.X, e.Y); 
+                UnvalidatedPixel.Add(upix);
+                pixelCount++;
+                pixelCountInfo.Text = pixelCount.ToString();
+            }
+            else
+            {
+                if ( ctrl_pressed && _MouseDown)
+                {
+                    if ( SelectPOINTA == null)
+                    {
+                        SelectPOINTA = new PixelWrapper.Vector(e.X, e.Y);
+                    }
+                    
+                    SelectPOINTB = new PixelWrapper.Vector(e.X, e.Y);
+                    ClearDraw();
+                    g.FillRectangle(Brushes.Blue, SelectPOINTA.X, SelectPOINTA.Y, SelectPOINTB.X - SelectPOINTA.X, SelectPOINTB.Y - SelectPOINTA.Y);
+                    PrintAllPixel();
+                    drawBox.Image = myDraw;
+                   
+                }
+            }
+        }
+
+        private void checkpixelstateBTN_Click(object sender, EventArgs e)
+        {
+            PrintValidatedPixel();
+        }
+    }
+}
