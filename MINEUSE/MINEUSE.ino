038197b7ec6a66699558f455a508f2577e9f6d3e
gggraph <62741099+gggraph@users.noreply.github.com>
2021-03-05 20:54:41 +0100

    ajout minage (sans tx)

--- MINEUSE/MINEUSE.ino
+++ MINEUSE/MINEUSE.ino
@@ -61,8 +61,9 @@ void printHash(uint8_t* hash) {
   :: index -> hash -> previoushash -> txnumb (max numb for atmega328p needed)-> txs -> timestamp??? -> MinerToken -> HT??? -> Nonce ???
   4     -> 32   -> 32           -> 1                                    -> ?*152 ->  4???       ->  40   ->  ???  -> ???
   */
-  // new stuff : blockchain file is unique. Tx limit per block is 4000 
-  // there is a new file: the "blockpointers" file. every new block, a new uint is written in it. it contains the pointer of the block newly wriiten.
+  // new stuff : block file are fixed size. there is one blockchain file which can get bigger. But every block are fixed size. Tx limit per block
+  // is 4000 
+  // there is a new file. the "blockpointers" file. every new block, a new uint is written in it. it contains the pointer of the block newly wriiten.
     
 #include <SPI.h>
 #include <SD.h>
@@ -82,9 +83,13 @@ void setup() {
 
   Serial.begin(9600);
   if (!InitializeSD()) return; 
-  ClearAllFiles();
+  Initchain();
   CheckFilesAtRoot();
   Test_PrintGenesisUTXOPAndMiningReward();
+  byte pkey[32]; // full of zero :: should be equal to Genesis pkey
+  while (1){
+    Mine(pkey,4); 
+  }
 }
 
 bool InitializeSD(){
@@ -96,6 +101,7 @@ bool InitializeSD(){
   return true;
 
 }
+/*
 void printDirectory(File *dir, int numTabs) { // will return the number of gile 
 
   while (true) {
@@ -110,6 +116,7 @@ void printDirectory(File *dir, int numTabs) { // will return the number of gile
     entry.close();
   }
 }
+*/
 void CheckFilesAtRoot(){
   
    // blockchain folder check 
@@ -157,19 +164,19 @@ void Initchain(){
   
    if (SD.exists("blockchain")){
     Serial.println("deleting blockchain file...");
-    SFILE = SD.remove("blockchain", FILE_WRITE); 
+    //SFILE = SD.remove("blockchain", FILE_WRITE); 
   }
   if (SD.exists("ptx")){
     Serial.println("deleting pending transaction file...");
-    SFILE = SD.remove("ptx", FILE_WRITE); 
+    //SFILE = SD.remove("ptx", FILE_WRITE); 
   }
   if (SD.exists("utxos")){
     Serial.println("deleting utxo set file...");
-    SFILE = SD.remove("utxos", FILE_WRITE); 
+    //SFILE = SD.remove("utxos", FILE_WRITE); 
   }
   if (SD.exists("blockpointers")){
     Serial.println("deleting blockpointers file...");
-    SFILE = SD.remove("blockpointers", FILE_WRITE); 
+    //SFILE = SD.remove("blockpointers", FILE_WRITE); 
   }
   // deleting all fork before remove the dir 
   // deleting all net before remove the dir 
@@ -227,7 +234,7 @@ void CreateGenesis(){
     gen[i] = i; 
   }
   // hash the stuff
-  sha.write(gen,520);
+  sha.write(gen,255);
   hash = sha.result();
   /* block struct : using SHA256 (32o for every hash). using Ed25519 (elliptic curve modulo 2^255 - 19) 32o private. 32o public. 64o sign
   :: index -> hash -> previoushash -> txnumb (max numb for atmega328p needed)-> txs -> timestamp??? -> MinerToken -> HT??? -> Nonce ???
@@ -260,6 +267,82 @@ void CreateGenesis(){
   SFILE = SD.open("blockchain", FILE_WRITE); 
   SFILE.seek(EOF);
   SFILE.write(genesis, 113);
+  SFILE.close();
+}
+uint32_t RequestLatestBlockIndex(){
+  // latest blockIndex is equal to blockpointers size / 4 
+  SFILE = SD.open("blockchain", FILE_WRITE); // genesis file is 113 o
+  unsigned long fsize = SFILE.size();
+  fsize /= 4 ;
+  return (uint32_t) fsize;  
+}
+// some verif for blocks ( because we cant load in ram ... ) 
+void GetHashAtBlockIndex(uint32_t index, byte *buff ){ 
+  
+  SFILE = SD.open("blockpointers", FILE_READ); // genesis file is 113 o
+  SFILE.seek(index*4); // go to last 4 bytes... 
+  byte uintbuff[4];
+  SFILE.read(uintbuff,4);
+  SFILE.close();
+  uint32_t bpointer = BytesToUint(uintbuff); 
+    //---------->GO FOR HADH +4 TO +32°
+  SFILE = SD.open("blockchain", FILE_READ);
+  SFILE.seek(bpointer + 4 );
+  SFILE.read(buff,32);
+  SFILE.close();
+  
+   
+}
+
+void GetMiningReward(uint32_t latestIndex){
+
+  
+}
+void Mine(byte *pkey, uint32_t utxop){
+  
+  // preparing the next block 
+  // get latest index + 1 
+  // get prev hash 
+  //:: index -> hash -> previoushash -> txnumb (max numb for atmega328p needed)-> txs -> timestamp??? -> MinerToken -> HT??? -> Nonce ???
+  // miner token : pkey -> utxop -> mining reward ( dont fuck with tx for the moment ) 
+  byte nextblock[81];
+  byte uintbuff[4];
+  byte i; // iterator
+  uint32_t u = RequestLatestBlockIndex();
+  byte hash[32];
+  GetHashAtBlockIndex(u, hash); 
+  u++; // inc to get new index
+  byte byteOffset = 0;
+  UINT32toBytes(u, uintbuff );
+  
+  for (i = 0 ; i < 4; i++ ) { nextblock[byteOffset] = uintbuff[i]; byteOffset++;}
+  // current hash is done at the end
+  //byteOffset += 32;  ------------------------------------------------------------------------ !!!!!!
+  for (i = 0 ; i < 32; i++ ) { nextblock[byteOffset] = hash[i]; byteOffset++;} // the prevhash
+  nextblock[byteOffset] = 0; byteOffset++; // 0 for the tx numb because we dont give a shit
+  UINT32toBytes(0, uintbuff ); //zeroing timestamp because its not clear for bimer
+  for (i = 0 ; i < 4; i++ ) { nextblock[byteOffset] = 0; byteOffset++;}// timeStamp? : equal to 0 probably
+  //--> miner token 
+  for (i = 0 ; i < 32; i++ ) { nextblock[byteOffset] = pkey[i]; byteOffset++;} // pkey 
+  UINT32toBytes(utxop,uintbuff); 
+  for (i = 0 ; i < 4; i++ ) { nextblock[byteOffset] = uintbuff[i]; byteOffset++;} // utxo pointer 
+  // coin reward 
+   UINT32toBytes(50,uintbuff);  // just big shit we use 50 for the moment LOL
+  for (i = 0 ; i < 4; i++ ) { nextblock[byteOffset] = uintbuff[i]; byteOffset++;} // mining  reward 
+  // now do the hash of all of this 
+  uint8_t *merkleroot; // same as byte ( unsigned 8 bit ...
+  sha.write(merkleroot,520);
+  merkleroot = sha.result();
+  // rebuild it 
+  byte winnerblock[132];
+  byteOffset = 0;
+  for (i = 0 ; i < 4; i++ ) { winnerblock[byteOffset] = nextblock[i]; byteOffset++;}
+  for (i = 0 ; i < 32; i++ ) { winnerblock[byteOffset] = merkleroot[i]; } // no inc byteoffset here
+  for (i = 0 ; i < 77; i++ ) { winnerblock[byteOffset] = nextblock[byteOffset]; byteOffset++;}
+   // append the winner block
+  SFILE = SD.open("blockchain", FILE_WRITE); 
+  SFILE.seek(EOF);
+  SFILE.write(winnerblock, 113);
   SFILE.close();
 }
 void loop() {
